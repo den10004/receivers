c70688f15cfd8824b402ca08737e4f15f1ddc6ba
cproam <gdocs34@gmail.com>
2024-11-29 16:34:59 +0300

    переделана шапка

--- sendforms.php
+++ sendforms.php
@@ -1,10 +1,11 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $to = "examples23@yandex.ru";
+    $subject = "weqw";
     $name = $_POST['name'];
     $tel = $_POST['tel'];
     echo "Данные успешно получены! Имя: $name, Email: $email";
-    if (mail($to, $name, $tel)) {
+    if (mail($to, $subject, $name . $tel)) {
         echo "Письмо успешно отправлено!";
     }
 } else {
