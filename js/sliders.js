99c8355ea608413e67665edfb593edaa11cbd9b6
cproam <gdocs34@gmail.com>
2024-11-29 16:55:56 +0300

    страница кофидециальности

---
+++ js/sliders.js
@@ -0,0 +1,143 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper3 = new Swiper(".mySwiper3", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper4 = new Swiper(".mySwiper4", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper3,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper5 = new Swiper(".mySwiper5", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper6 = new Swiper(".mySwiper6", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper7 = new Swiper(".mySwiper7", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper8 = new Swiper(".mySwiper8", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper7,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper9 = new Swiper(".mySwiper9", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper10 = new Swiper(".mySwiper10", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper9,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper11 = new Swiper(".mySwiper11", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper12 = new Swiper(".mySwiper12", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper11,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper13 = new Swiper(".mySwiper13", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper14 = new Swiper(".mySwiper14", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper13,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper15 = new Swiper(".mySwiper15", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper16 = new Swiper(".mySwiper16", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper15,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper17 = new Swiper(".mySwiper17", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper18 = new Swiper(".mySwiper18", {
+  spaceBetween: 10,
+  thumbs: {
+    swiper: swiper17,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
