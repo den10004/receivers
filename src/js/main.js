9ad13823dcb7b45820d89633bf826544b4eb468e
cproam <gdocs34@gmail.com>
2024-12-03 12:19:39 +0300

    правки

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,17 @@
-/*import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
-*/
-
 window.addEventListener("vite:preloadError", (e) => {
   window.reload();
 });
 
 function toggleBlock(blockId) {
   const block = document.getElementById(blockId);
+  console.log(block);
 
   if (block.classList.contains("show")) {
     block.classList.remove("show");
-    block.previousElementSibling.innerHTML = `Читать полностью <span class="readAll"></span>`;
+    block.nextElementSibling.innerHTML = `Читать полностью <span class="readAll"></span>`;
   } else {
     block.classList.add("show");
-    block.previousElementSibling.innerHTML = `Свернуть <span class="readAll-rotate"></span>`;
+    block.nextElementSibling.innerHTML = `Свернуть <span class="readAll-rotate"></span>`;
   }
 }
 
